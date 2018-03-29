6dada5b2035716e8bd366ba759923864ef200b4b
rkritchat <rojanaphruk.r@gmail.com>
2018-03-29 18:21:51 +0700

    Delete class not use

--- src/test/java/com/spring/bootjdbc/RegisterTest.java
+++ src/test/java/com/spring/bootjdbc/RegisterTest.java
@@ -8,16 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,14 +20,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class RegisterTest {
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
     private RegisterValidator validator;
 
     @Test
-    public void createUser_exist_mustError() throws Exception {
-        UserInfo data = new UserInfo("user","earl");
+    public void createUser_exist_mustError() {
+        UserInfo data = new UserInfo("user", "earl");
         data.setName("");
         Errors errors = new BeanPropertyBindingResult(data, "userInfo");
         validator.validate(data, errors);
